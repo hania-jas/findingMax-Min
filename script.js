3d1c427fc214489748e3350d896b1c312dfb8b98
hania-jas <60855635+hania-jas@users.noreply.github.com>
2020-07-16 16:17:00 +0200

    created script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const buttonMin = document.getElementById('buttonMin');
 const buttonMax = document.getElementById('buttonMax');
 let inputNumbers = [];
 const findMin = () => {
-    input.innerHTML =
+
 }
