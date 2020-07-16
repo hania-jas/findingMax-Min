720c3cce4ff1f70cc3ba4cfe0c3c4cb2a37f546d
hania-jas <60855635+hania-jas@users.noreply.github.com>
2020-07-16 16:11:12 +0200

    created script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+let input = document.getElementById('input');
+const buttonMin = document.getElementById('buttonMin');
+const buttonMax = document.getElementById('buttonMax');
+let inputNumbers = [];
+const findMin = () => {
+    input.innerHTML =
+}
