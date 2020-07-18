aae2c34070085cb07bd8da0ae0bea70de845f582
hania-jas <60855635+hania-jas@users.noreply.github.com>
2020-07-18 21:26:49 +0200

    added function createNumbers

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 let input = document.getElementById('input');
 const buttonMin = document.getElementById('buttonMin');
 const buttonMax = document.getElementById('buttonMax');
-let inputNumbers = [];
 
-const findMin = () => {
+const createNumbers = (e) => {
+    let clickedValue = e.key;
+    const inputValue = input.value + clickedValue;
+    console.log(inputValue);
+    const array = inputValue.split(",");
+    console.log(array);
 
+    // if (isFinite(clickedValue)) {
+    // const array = string.split(" ,");
+
+    //} else {
+    //     // clean();
+    //     // alert('Only numbers accepted!')
+
+    // }
 }
+
+const clean = () => {
+        inputNumbers = [];
+        input.value = '';
+    }
+    // console.log(inputNumbers);
+document.addEventListener('keydown', createNumbers)
