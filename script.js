2688b0d2777a58cc7edd8f78310f72dbd9b980d8
hania-jas <60855635+hania-jas@users.noreply.github.com>
2020-07-19 16:00:22 +0200

    added event listeners to buttons

--- script.js
+++ script.js
@@ -1,40 +1,29 @@
-let input = document.getElementById('input');
+const input = document.getElementById('userNumbersInput');
 const buttonMin = document.getElementById('buttonMin');
-let buttonMax = document.getElementById('buttonMax');
+const buttonMax = document.getElementById('buttonMax');
 
-const createNumbers = (e) => {
-    let clickedValue = e.key;
-    let inputValue = input.value + clickedValue;
-    console.log(inputValue);
-    let array = inputValue.split(",");
-    console.log(array.sort(minToMax));
 
-    if (buttonMax.onclick) {
-        findMax(array);
-    }
-
-    // if (isFinite(clickedValue)) {
-    // const array = string.split(" ,");
-
-    //} else {
-    //     // alert('Only numbers accepted!')
-
-    // }
+const getSplitedNumbersFromInput = () => {
+    let inputValue = input.value;
+    let arr = inputValue.split(",");
+    return arr.sort(minToMax);
 
 }
 const minToMax = (a, b) => {
     return a - b;
-
-
 }
 
-const findMax = (arr) => {
+const findMaxNumber = () => {
+    arr = getSplitedNumbersFromInput();
     let max = arr[arr.length - 1];
     buttonMax.innerText = max;
-    console.log('max', max);
+
 }
-const findMin = (arr) => {
+const findMinNumber = () => {
+    arr = getSplitedNumbersFromInput();
     let min = arr[0];
     buttonMin.innerText = min;
 }
-document.addEventListener('keydown', createNumbers);
+
+buttonMax.addEventListener('click', findMaxNumber);
+buttonMin.addEventListener('click', findMinNumber);
