f42dae703d9234ca421589498cce64c7a08eab37
hania-jas <60855635+hania-jas@users.noreply.github.com>
2020-07-24 20:52:12 +0200

    finding minVal and maxVal with for loop

--- script.js
+++ script.js
@@ -5,24 +5,32 @@ const buttonMax = document.getElementById('buttonMax');
 
 const getSplitedNumbersFromInput = () => {
     const inputValue = input.value;
-    const arr = inputValue.split(",");
-    return arr.sort(minToMax);
-
-}
-const minToMax = (a, b) => {
-    return a - b;
+    const arr = inputValue.split(",").map(Number);
+    console.log(arr)
+    let minVal = 9999999;
+    let maxVal = -99999999999;
+    console.log(arr);
+    for (let i = 0; i < arr.length; i++) {
+        console.log('arr[i]', arr[i]);
+        if (arr[i] < minVal) {
+            minVal = arr[i];
+        }
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+        console.log('minVal', minVal);
+        console.log('maxVal', maxVal);
+    }
+    return [minVal, maxVal];
 }
 
 const findMaxNumber = () => {
-    arr = getSplitedNumbersFromInput();
-    const max = arr[arr.length - 1];
-    buttonMax.innerText = max;
-
+    const maxVal = getSplitedNumbersFromInput();
+    buttonMax.innerText = maxVal[1];
 }
 const findMinNumber = () => {
-    arr = getSplitedNumbersFromInput();
-    const min = arr[0];
-    buttonMin.innerText = min;
+    const minVal = getSplitedNumbersFromInput();
+    buttonMin.innerText = minVal[0];
 }
 
 buttonMax.addEventListener('click', findMaxNumber);
